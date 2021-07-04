7ee49cff7a3be2d210d1da76e8c57079250f85d0
Manak Mishra <18ucc104@lnmiit.ac.in>
2021-07-04 19:14:32 +0530

    added navbar to wip page

--- pages/wip.tsx
+++ pages/wip.tsx
@@ -5,18 +5,21 @@ import {
     Text
 } from '@chakra-ui/react';
 import { Construction } from '../components/svg';
+import Container from '../components/Container';
 
 const UnderConstruction: React.FC = () => {
     return (
         <>
-            <Center h="80vh" w="100vw">
-                <VStack textAlign="center" spacing="4">
-                    <Construction boxSize="8rem" />
-                        <Text textStyle="wip">
-                            This site is currently under construction.
-                        </Text>
-                </VStack>
-            </Center>
+            <Container>
+                <Center h="80vh" w="100vw">
+                    <VStack textAlign="center" spacing="4">
+                        <Construction boxSize="8rem" />
+                            <Text textStyle="wip">
+                                This site is currently under construction.
+                            </Text>
+                    </VStack>
+                </Center>
+            </Container>
         </>
     );
 } 
